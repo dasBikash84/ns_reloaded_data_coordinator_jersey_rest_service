d12397eb0a60329ebdd5a16f636ab28b921d272f
Bikash Das <bikashdaseee@gmail.com>
2019-05-20 17:07:33 +0600

    'NewsPaperController' added.

---
+++ src/main/kotlin/com/dasbikash/news_server_data_coordinator_rest_jersey/jersey/rest_controllers/NewsPaperController.kt
@@ -0,0 +1,46 @@
+package com.dasbikash.news_server_data_coordinator_rest_jersey.jersey.rest_controllers
+
+import com.dasbikash.news_server_data_coordinator_rest_jersey.model.Newspapers
+import com.dasbikash.news_server_data_coordinator_rest_jersey.model.RequestDetailsBean
+import com.dasbikash.news_server_data_coordinator_rest_jersey.services.NewsPaperService
+import com.dasbikash.news_server_data_coordinator_rest_jersey.utills.RestControllerUtills
+import org.springframework.stereotype.Component
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+@Path("newspapers")
+@Produces(value = arrayOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+@Component
+open class NewsPaperController
+constructor(open var newsPaperService: NewsPaperService?=null,
+            open var restControllerUtills: RestControllerUtills?=null) {
+
+    @GET
+    @Path("")
+    @Produces(value = arrayOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+    open fun getAllActiveNewsPapersEndPoint(@BeanParam requestDetails: RequestDetailsBean): Response {
+        return restControllerUtills!!.entityToResponseEntity(
+                            Newspapers(newsPaperService!!.getAllActiveNewsPapers()))
+    }
+
+    @GET
+    @Path("/country-name/{countryName}")
+    @Produces(value = arrayOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+    open fun getNewPaperByCountryNameEndPoint(@PathParam("countryName") countryName:String,
+                                              @BeanParam requestDetails: RequestDetailsBean)
+            :Response{
+        return restControllerUtills!!.entityToResponseEntity(Newspapers(
+                newsPaperService!!.getAllNewPaperByCountryName(countryName)))
+    }
+
+    @GET
+    @Path("/language-id/{languageId}")
+    @Produces(value = arrayOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+    open fun getNewPaperByLanguageIdEndPoint(@PathParam("languageId") languageId:String,
+                                             @BeanParam requestDetails: RequestDetailsBean)
+            :Response{
+        return restControllerUtills!!.entityToResponseEntity(
+                    Newspapers(newsPaperService!!.getAllNewPaperByLanguageId(languageId)))
+    }
+}
