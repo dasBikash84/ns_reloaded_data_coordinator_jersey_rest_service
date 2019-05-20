0d4bab885687bd421e67176cc25c2e937434947b
Bikash Das <bikashdaseee@gmail.com>
2019-05-20 16:42:31 +0600

    'CountryController' added.

---
+++ src/main/kotlin/com/dasbikash/news_server_data_coordinator_rest_jersey/jersey/rest_controllers/CountryController.kt
@@ -0,0 +1,29 @@
+package com.dasbikash.news_server_data_coordinator_rest_jersey.jersey.rest_controllers
+
+import com.dasbikash.news_server_data_coordinator_rest_jersey.model.Countries
+import com.dasbikash.news_server_data_coordinator_rest_jersey.model.RequestDetailsBean
+import com.dasbikash.news_server_data_coordinator_rest_jersey.services.CountryService
+import com.dasbikash.news_server_data_coordinator_rest_jersey.utills.RestControllerUtills
+import org.springframework.stereotype.Component
+import javax.ws.rs.BeanParam
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+@Path("countries")
+@Produces(value = arrayOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+@Component
+open class CountryController
+constructor(open var countryService: CountryService?=null,
+            open var restControllerUtills: RestControllerUtills?=null) {
+
+    @GET
+    @Path("")
+    @Produces(value = arrayOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+    open fun getAllCountriesEndPoint(@BeanParam requestDetails: RequestDetailsBean):Response{
+        return restControllerUtills!!.entityToResponseEntity(Countries(countryService!!.getAllCountries()))
+    }
+
+}
